2921ad9f3e472ec39834e0f4961c5a106784b8df
Pavel Sudomoin <sudomoin.pavel@yandex.ru>
2020-07-01 14:05:24 +0300

    add admin.site.register for Comment

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Post, Group
+from .models import Post, Group, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -16,5 +16,13 @@ class GroupAdmin(admin.ModelAdmin):
     empty_value_display = "-пусто-"
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("pk", "text", "created", "author", "post")
+    search_fields = ("text",)
+    list_filter = ("created",)
+    empty_value_display = "-пусто-"
+
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Group, GroupAdmin)
+admin.site.register(Comment, CommentAdmin)
